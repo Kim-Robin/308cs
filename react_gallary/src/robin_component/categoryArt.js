56c1961976c26005bf6d6a59a82fb004f065c376
Kim-Robin <robin1265@gmail.com>
2020-08-17 15:45:08 -0400

    update for category art

--- react_gallary/src/robin_component/categoryArt.js
+++ react_gallary/src/robin_component/categoryArt.js
@@ -1,23 +1,57 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 export default class randomize extends Component{
 
     category = this.props.location.pathname.substring(7);
 
+    constructor(props){
+        super(props);
+
+        this.state ={
+            category : this.category,
+            like: [],
+            date: [],
+            file: []
+        }
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:5100/robin/'+ this.category)
+        .then(res=>{
+            this.setState({
+                like: res.data.map(value => value.like),
+                date:res.data.map(value => value.date),
+                file:res.data.map(value => value.file)
+            })
+        })
+
+        // console.log(this.state.file);
+    }
+
 
     
     render(){
-
+        // this.state.date.sort()
         console.log();
+
+        this.state.date.map(date =>
+            console.log(date));
         
 
         return(
             <div className="rpage">
-            <Link to="/robin/" className="redirect">
-                <img src="https://img.icons8.com/metro/50/000000/  left.png"/>
-            </Link>
+                <Link to="/robin/" className="redirect">
+                    <img src="https://img.icons8.com/metro/50/000000/  left.png"/>
+                </Link>
                 <h1 className="material">{this.category}</h1>
+                <div className="row justify-content-center">
+                    {this.state.file.map( image =>
+                        <h1 className="centerize col col-lg-3 col-md-4 col-sm-6">{image}</h1>
+                    )}
+                    {/* <img src="https://cnet3.cbsistatic.com/img/-qQkzFVyOPEoBRS7K5kKS0GFDvk=/940x0/2020/04/16/7d6d8ed2-e10c-4f91-b2dd-74fae951c6d8/bazaart-edit-app.jpg"></img> */}
+                </div>
             </div>
             
         )
